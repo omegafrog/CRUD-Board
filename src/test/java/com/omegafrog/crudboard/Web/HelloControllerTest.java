17978b764e8a92b20e77e60343c5d952d86508b0
omegafrog <jiwoo22110103@gmail.com>
2022-07-03 16:51:52 +0900

    롬복으로 dto 만들기

--- src/test/java/com/omegafrog/crudboard/Web/HelloControllerTest.java
+++ src/test/java/com/omegafrog/crudboard/Web/HelloControllerTest.java
@@ -1,4 +1,4 @@
-package com.omegafrog.crudboard.web;
+package com.omegafrog.crudboard.Web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,9 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 // 테스트를 실행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행.
 // SpringRunner라는 스프링 부트 테스트와 Junit 사이에 연결자 역할.
@@ -25,5 +25,16 @@ public class HelloControllerTest {
         String hello = "hello";
 
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+    }
+
+    @Test
+    public void HelloDto가_리턴된다() throws Exception{
+        String name = "name";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name",name).param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
